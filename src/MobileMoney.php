82463f0ff3006454f25ed16b9f3462cceb967c75
Joseph M. Matembu <jmatembu@gmail.com>
2019-02-11 14:17:58 +0300

    Request payment - Check if payment was successful - Validate request data

--- src/MobileMoney.php
+++ src/MobileMoney.php
@@ -3,10 +3,21 @@
 namespace Ssemco\MobileMoney;
 
 use GuzzleHttp\Client;
+use Illuminate\Support\Str;
 
 class MobileMoney
 {
     protected $client;
+    protected $paymentRequestOptions = [
+        'amount',
+        'currency',
+        'externalId',
+        'payerType',
+        'payerId',
+        'payerMessage',
+        'payeeMessage'
+    ];
+    protected $missingKeys = [];
 
     public function __construct(Client $client)
     {
@@ -25,7 +36,11 @@ class MobileMoney
                 ]
             ]);
 
-            return collect(json_decode($response->getBody()->getContents(), true));
+            $contents = $this->getResponseBody($response);
+
+            $token = $contents->get('access_token');
+
+            return $token;
 
         } catch (\Exception $e) {
             throw new \Exception("Could not get access token", 500);
@@ -33,20 +48,98 @@ class MobileMoney
 
     }
 
-    public function pay()
+    public function request(array $options)
     {
-        $accessToken = $this->accessToken();
+        if (! $this->hasValidKeys($options)) {
+            throw new \Exception("Missing required key: " . $this->missingKeys[0], 400);
+        }
 
+        $this->referenceId = (string) Str::uuid();
+        $this->accessToken = $this->accessToken();
+        // Request the payment
         $response = $this->client->post('https://ericssonbasicapi2.azure-api.net/collection/v1_0/requesttopay',
             [
                 'headers' => [
-                    'X-Reference-Id' => config('mobilemoney.mtn.user_id'),
+                    'Authorization' => 'Bearer ' . $this->accessToken,
+                    'X-Reference-Id' => $this->referenceId,
                     'X-Target-Environment' => 'sandbox',
-                    'Ocp-Apim-Subscription-Key' => config('mobilemoney.mtn.primary_key')
+                    'Ocp-Apim-Subscription-Key' => config('mobilemoney.mtn.primary_key'),
+                ],
+                'json' => [
+                    'amount' => $options['amount'] ,
+                    'currency' => $options['currency'],
+                    'externalId' => $options['externalId'],
+                    'payer' => [
+                        'partyIdType' => $options['payerType'],
+                        'partyId' => $options['payerId']
+                    ],
+                    'payerMessage' => $options['payerMessage'],
+                    'payeeNote' => $options['payeeMessage']
                 ]
             ]
         );
 
-        return collect($response->getBody()->getContents());
+        return collect([
+            'reference_id' => $this->referenceId,
+            'access_token' => $this->accessToken
+        ]);
+    }
+
+    public function requestStatus(string $referenceId)
+    {
+        try {
+            $response = $this->client->get(
+                'https://ericssonbasicapi2.azure-api.net/collection/v1_0/requesttopay/' . $referenceId,
+                [
+                    'headers' => [
+                        'Authorization' => 'Bearer ' . $this->accessToken(),
+                        'X-Target-Environment' => 'sandbox',
+                        'Ocp-Apim-Subscription-Key' => config('mobilemoney.mtn.primary_key'),
+                    ]
+                ]
+            );
+
+            return $this->getResponseBody($response);
+
+        } catch (\Exception $e) {
+
+            return collect(['error' => $e->getMessage()]);
+        }
+
+    }
+
+    public function paymentSuccessful(string $referenceId) : bool
+    {
+        try {
+            $payment = $this->requestStatus($referenceId);
+
+            return strtoupper($payment->get('status', 'FAIL')) === 'SUCCESSFUL';
+
+        } catch (\Exception $e) {
+            return false;
+        }
+
+    }
+
+    protected function missingKeys(array $source) : array {
+        $missingKeys = array_diff($this->paymentRequestOptions, array_keys($source));
+
+        $this->missingKeys = array_values($missingKeys);
+
+        return $this->missingKeys;
+    }
+
+    protected function hasValidKeys(array $source)
+    {
+        if (count($this->missingKeys($source)) === 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    protected function getResponseBody($response)
+    {
+        return collect(json_decode($response->getBody()->getContents(), true));
     }
 }
